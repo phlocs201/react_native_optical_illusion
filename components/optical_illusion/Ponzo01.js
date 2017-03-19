c0a649b3509f85189bddfbe18af0d37c9bf5f43a
jumtech <despairing.clock+jumGithub@gmail.com>
2017-03-19 19:47:50 +0900

    ポンゾ錯視にアニメーションを付与

--- components/optical_illusion/Ponzo01.js
+++ components/optical_illusion/Ponzo01.js
@@ -1,50 +1,119 @@
 import React, { Component } from 'react';
-import Svg,{
-    Line,
-} from 'react-native-svg';
+import { View, Button, Animated, Text } from 'react-native';
+import Svg, { Line } from 'react-native-svg';
+
+const AnimatedLine = Animated.createAnimatedComponent(Line);
 
 export default class Ponzo01 extends Component {
-  render() {
-    return (
-      <Svg
-        height="100"
-        width="110"
-      >
-        {/* 外側の < */}
-        <Line
-          x1="10"
-          y1="50"
-          x2="110"
-          y2="0"
-          stroke="black"
-          strokeWidth="2"
-        />
-        <Line
-          x1="10"
-          y1="50"
-          x2="110"
-          y2="100"
-          stroke="black"
-          strokeWidth="2"
-        />
-        {/* 内側の || */}
-        <Line
-          x1="60"
-          y1="30"
-          x2="60"
-          y2="70"
-          stroke="black"
-          strokeWidth="2"
-        />
-        <Line
-          x1="105"
-          y1="30"
-          x2="105"
-          y2="70"
-          stroke="black"
-          strokeWidth="2"
-        />
-      </Svg>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            opacityAnim: new Animated.Value(0),
+            leftAnim: new Animated.Value(0),
+        };
+    }
+
+    _handleStartAnimation() {
+        Animated.sequence([
+            Animated.timing(
+                this.state.opacityAnim,
+                {toValue: 1, duration: 2000},
+            ),
+            Animated.timing(
+                this.state.leftAnim,
+                {toValue: 1, duration: 2000},
+            ),
+        ]).start();
+    }
+
+    render() {
+        const opacity1to0 = this.state.opacityAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0]
+        });
+        const left180to90 = this.state.leftAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [180, 90]
+        });
+
+        return (
+          <View style={{
+            width: 200,
+            height: 200,
+            margin: 20,
+            position:'absolute',
+          }}>
+            <View>
+              <Svg
+                height="200"
+                width="200"
+              >
+                {/* 外側の < */}
+                <AnimatedLine
+                  x1="0"
+                  y1="100"
+                  x2="200"
+                  y2="0"
+                  stroke="black"
+                  strokeWidth="2"
+                  opacity={opacity1to0}
+                />
+                <AnimatedLine
+                  x1="0"
+                  y1="100"
+                  x2="200"
+                  y2="200"
+                  stroke="black"
+                  strokeWidth="2"
+                  opacity={opacity1to0}
+                />
+              </Svg>
+            </View>
+            <View style={{
+              position:'absolute',
+              top: 60,
+              left: 90,
+            }}>
+              <Svg
+                height="100"
+                width="10"
+              >
+                {/* 左の || */}
+                <Line
+                  x1="0"
+                  y1="0"
+                  x2="0"
+                  y2="80"
+                  stroke="black"
+                  strokeWidth="4"
+                />
+              </Svg>
+            </View>
+            <Animated.View style={{
+              position:'absolute',
+              top: 60,
+              left: left180to90,
+            }}>
+              <Svg
+                height="100"
+                width="10"
+              >
+                {/* 右の | */}
+                <Line
+                  x1="0"
+                  y1="0"
+                  x2="0"
+                  y2="80"
+                  stroke="black"
+                  strokeWidth="4"
+                />
+              </Svg>
+            </Animated.View>
+            <Button
+                onPress={() => this._handleStartAnimation()}
+                title="結果を見る"
+            />
+          </View>
+        );
+    }
 }
--- components/optical_illusion/Ponzo02.js
+++ components/optical_illusion/Ponzo02.js
@@ -1,51 +1,120 @@
 import React, { Component } from 'react';
-import Svg,{
-    Line,
-    Circle,
-} from 'react-native-svg';
+import { View, Button, Animated, Text } from 'react-native';
+import Svg, { Line, Circle } from 'react-native-svg';
 
-export default class Ponzo02 extends Component {
-  render() {
-    return (
-      <Svg
-        height="100"
-        width="110"
-      >
-        {/* 外側の < */}
-        <Line
-          x1="10"
-          y1="50"
-          x2="110"
-          y2="0"
-          stroke="black"
-          strokeWidth="2"
-        />
-        <Line
-          x1="10"
-          y1="50"
-          x2="110"
-          y2="100"
-          stroke="black"
-          strokeWidth="2"
-        />
-        {/* 内側の ○ ○ */}
-        <Circle
-          cx="50"
-          cy="50"
-          r="13"
-          stroke="black"
-          strokeWidth="2"
-          fill="none"
-        />
-        <Circle
-          cx="95"
-          cy="50"
-          r="13"
-          stroke="black"
-          strokeWidth="2"
-          fill="none"
-        />
-      </Svg>
-    );
-  }
+const AnimatedLine = Animated.createAnimatedComponent(Line);
+
+export default class Ponzo01 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            opacityAnim: new Animated.Value(0),
+            leftAnim: new Animated.Value(0),
+        };
+    }
+
+    _handleStartAnimation() {
+        Animated.sequence([
+            Animated.timing(
+                this.state.opacityAnim,
+                {toValue: 1, duration: 2000},
+            ),
+            Animated.timing(
+                this.state.leftAnim,
+                {toValue: 1, duration: 2000},
+            ),
+        ]).start();
+    }
+
+    render() {
+        const opacity1to0 = this.state.opacityAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0]
+        });
+        const left120to25 = this.state.leftAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [120, 25]
+        });
+
+        return (
+          <View style={{
+            width: 200,
+            height: 200,
+            margin: 20,
+            position:'absolute',
+          }}>
+            <View>
+              <Svg
+                height="200"
+                width="200"
+              >
+                {/* 外側の < */}
+                <AnimatedLine
+                  x1="0"
+                  y1="100"
+                  x2="200"
+                  y2="0"
+                  stroke="black"
+                  strokeWidth="2"
+                  opacity={opacity1to0}
+                />
+                <AnimatedLine
+                  x1="0"
+                  y1="100"
+                  x2="200"
+                  y2="200"
+                  stroke="black"
+                  strokeWidth="2"
+                  opacity={opacity1to0}
+                />
+              </Svg>
+            </View>
+            <View style={{
+              position:'absolute',
+              top: 50,
+              left: 25,
+            }}>
+              <Svg
+                height="100"
+                width="100"
+              >
+                {/* 左の || */}
+                <Circle
+                  cx="50"
+                  cy="50"
+                  r="30"
+                  stroke="black"
+                  strokeWidth="2"
+                  fill="none"
+                />
+              </Svg>
+            </View>
+            <Animated.View style={{
+              position:'absolute',
+              top: 50,
+              left: left120to25,
+            }}>
+              <Svg
+                height="100"
+                width="100"
+              >
+                {/* 右の | */}
+              <Circle
+                cx="50"
+                cy="50"
+                r="30"
+                stroke="black"
+                strokeWidth="2"
+                fill="none"
+              />
+              </Svg>
+            </Animated.View>
+            <Button
+                onPress={() => this._handleStartAnimation()}
+                title="結果を見る"
+            />
+          </View>
+        );
+    }
 }
+
