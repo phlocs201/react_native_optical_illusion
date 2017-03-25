ec4e94ca8fb6267dbf19d5bc093bbd3ebd64cf3c
jumtech <despairing.clock+jumGithub@gmail.com>
2017-03-25 15:50:33 +0900

    ポンゾ錯視1のアニメーションが完成

--- components/optical_illusion/Ponzo01.js
+++ components/optical_illusion/Ponzo01.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, Animated, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { View, Button, Animated, Text, TouchableWithoutFeedback, StyleSheet, Image } from 'react-native';
 import Svg, { Line } from 'react-native-svg';
 
 const AnimatedLine = Animated.createAnimatedComponent(Line);
@@ -8,38 +8,50 @@ export default class Ponzo01 extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        opacityAnim: new Animated.Value(0),
+        linesOpacity: new Animated.Value(0),
+        wholeOpacity: new Animated.Value(0),
         topAnim: new Animated.Value(0),
-        phase: 0,
+        illusionPhase: 0,
+        descPhase: 0,
         isPressed: false,
       };
       this._nextPhase = this._nextPhase.bind(this);
       this._previousPhase = this._previousPhase.bind(this);
     }
-
     _nextPhase() {
-      const phase = this.state.phase;
-        if (phase === 0) {
+      const { illusionPhase, descPphase } = this.state;
+        if (illusionPhase === 0) {
           this.setState({ isPressed: true });
           Animated.sequence([
             Animated.timing(
-              this.state.opacityAnim,
+              this.state.wholeOpacity,
               {toValue: 1, duration: 2000},
             ),
             Animated.timing(
               this.state.topAnim,
               {toValue: 1, duration: 2000},
             ),
-          ]).start(() => this.setState({
-            phase: 1,
-          }));
-        } else if (phase === 1) {
-            this.setState({phase: 2})
+          ]).start(() => this.setState({illusionPhase: 1, descPhase: 1}));
+        } else if (illusionPhase === 1) {
+          Animated.sequence([
+            Animated.timing(
+              this.state.topAnim,
+              {toValue: 0, duration: 1000},
+            ),
+            Animated.timing(
+              this.state.linesOpacity,
+              {toValue: 0, duration: 1000},
+            ),
+            Animated.timing(
+              this.state.wholeOpacity,
+              {toValue: 1, duration: 3000},
+            ),
+          ]).start(() => this.setState({illusionPhase: 2, descPhase: 2}));
         }
     }
     _previousPhase() {
-      const phase = this.state.phase;
-        if (phase === 1) {
+      const { illusionPhase, descPphase } = this.state;
+        if (illusionPhase === 1) {
           this.setState({ isPressed: true });
           Animated.sequence([
             Animated.timing(
@@ -47,20 +59,39 @@ export default class Ponzo01 extends Component {
               {toValue: 0, duration: 0},
             ),
             Animated.timing(
-              this.state.opacityAnim,
+              this.state.linesOpacity,
               {toValue: 0, duration: 0},
             ),
-          ]).start(() => this.setState({
-            phase: 0,
-          }));
-        } else if (phase === 2) {
-            this.setState({phase: 1})
+          ]).start(() => this.setState({illusionPhase: 0}));
+        } else if (illusionPhase === 2) {
+          Animated.sequence([
+            Animated.timing(
+              this.state.topAnim,
+              {toValue: 1, duration: 0},
+            ),
+            Animated.timing(
+              this.state.linesOpacity,
+              {toValue: 1, duration: 0},
+            ),
+            Animated.timing(
+              this.state.illusionOpacity,
+              {toValue: 0, duration: 0},
+            ),
+          ]).start(() => this.setState({illusionPhase: 1}));
         }
     }
 
     render() {
-        const {phase} = this.state;
-        const opacity1to0 = this.state.opacityAnim.interpolate({
+        const { illusionPhase, descPhase } = this.state;
+        const linesOpacity = this.state.linesOpacity.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0]
+        });
+        const imageOpacity = this.state.wholeOpacity.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1]
+        });
+        const illusionOpacity = this.state.wholeOpacity.interpolate({
             inputRange: [0, 1],
             outputRange: [1, 0]
         });
@@ -68,19 +99,24 @@ export default class Ponzo01 extends Component {
             inputRange: [0, 1],
             outputRange: [180, 90]
         });
-
         return (
           <View style={styles.containerView}>
             <View style={styles.figureView}>
-            <View style={styles.outerSvg}>
+            <Animated.View style={{opacity: imageOpacity}}>
+              <Image
+                 style={styles.image}
+                 source={require('../../images/rail_way.jpg')}
+              />
+              </Animated.View>
+            <Animated.View style={[styles.outerSvg, {opacity: illusionOpacity}]}>
               <View>
                 <Svg height="200" width="200">
                   {/* 外側の < */}
-                  <AnimatedLine x1="100" y1="0" x2="0" y2="200" stroke="black" strokeWidth="2" opacity={opacity1to0} />
-                  <AnimatedLine x1="100" y1="0" x2="200" y2="200" stroke="black" strokeWidth="2" opacity={opacity1to0} />
+                  <AnimatedLine x1="100" y1="0" x2="0" y2="200" stroke="black" strokeWidth="2" opacity={linesOpacity} />
+                  <AnimatedLine x1="100" y1="0" x2="200" y2="200" stroke="black" strokeWidth="2" opacity={linesOpacity} />
                 </Svg>
               </View>
-              <Animated.View style={[styles.text_a, {opacity: opacity1to0}]}>
+              <Animated.View style={[styles.text_a, {opacity: linesOpacity}]}>
                   <Text style={styles.abText}>A</Text>
               </Animated.View>
               <View style={styles.stickSvg}>
@@ -89,7 +125,7 @@ export default class Ponzo01 extends Component {
                   <Line x1="0" y1="0" x2="80" y2="0" stroke="black" strokeWidth="4" />
                 </Svg>
               </View>
-              <Animated.View style={[styles.text_b, {opacity: opacity1to0}]}>
+              <Animated.View style={[styles.text_b, {opacity: linesOpacity}]}>
                   <Text style={styles.abText}>B</Text>
               </Animated.View>
               <Animated.View style={[styles.stickSvg, {top: top180to90}]}>
@@ -98,13 +134,14 @@ export default class Ponzo01 extends Component {
                   <Line x1="0" y1="0" x2="80" y2="0" stroke="black" strokeWidth="4" />
                 </Svg>
               </Animated.View>
-            </View>
+            </Animated.View>
             </View>
             <View style={styles.descriptionView}>
-              <Description phase={phase} />
+              <Description  descPhase={descPhase}/>
             </View>
             <Footer
-              phase={phase}
+              illusionPhase={illusionPhase}
+              descPhase={descPhase}
               _nextPhase={this._nextPhase}
               _previousPhase={this._previousPhase}
             />
@@ -115,8 +152,8 @@ export default class Ponzo01 extends Component {
 
 class Description extends Component {
   render() {
-    const { phase } = this.props;
-    if (phase === 0) {
+    const { descPhase } = this.props;
+    if (descPhase === 0) {
       return (
         <View>
           <Text style={styles.descText}>【質問】</Text>
@@ -124,7 +161,7 @@ class Description extends Component {
           <Text style={styles.descText}>どちらの棒の方が長く見えますか？</Text>
         </View>
       )
-    } else if (phase === 1) {
+    } else if (descPhase === 1) {
       return (
         <View>
           <Text style={styles.descText}>【解説】</Text>
@@ -132,7 +169,7 @@ class Description extends Component {
           <Text style={styles.descText}>人間は物体の大きさを背景に依存して判断していることを、示したポンゾ錯視といいます。</Text>
         </View>
       )
-    } else if (phase === 2) {
+    } else if (descPhase === 2) {
       return (
         <View>
           <Text style={styles.descText}>【解説】</Text>
@@ -145,8 +182,8 @@ class Description extends Component {
 
 class Footer extends Component {
   render() {
-    const { phase, handleFunc } = this.props;
-    if (phase === 0) {
+    const { illusionPhase, handleFunc } = this.props;
+    if (illusionPhase === 0) {
       return(
         <View style={styles.footerView}>
           <View style={styles.previousButton}/>
@@ -154,12 +191,12 @@ class Footer extends Component {
             <Button
               onPress={this.props._nextPhase}
               style={styles.button}
-              title="答え"
+              title="次へ"
             />
           </View>
         </View>
       )
-    } else if (phase === 1) {
+    } else if (illusionPhase === 1) {
       return(
         <View style={styles.footerView}>
           <View style={styles.previousButton}>
@@ -173,12 +210,12 @@ class Footer extends Component {
             <Button
               onPress={this.props._nextPhase}
               style={styles.button}
-              title="解説"
+              title="次へ"
             />
           </View>
         </View>
       )
-    } else if (phase === 2) {
+    } else if (illusionPhase === 2) {
       return(
         <View style={styles.footerView}>
           <View style={styles.previousButton}>
@@ -233,6 +270,13 @@ const styles = StyleSheet.create({
   descText: {
     fontSize: 20,
     textAlign: 'left',
+  },
+  image: {
+    position: 'absolute',
+    top: 90,
+    left: 60,
+    width: 200,
+    height: 200,
   },
   abText: {
     fontSize: 20,
