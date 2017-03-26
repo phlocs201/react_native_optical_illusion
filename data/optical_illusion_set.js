001147c927cb07ce57208b64f6a5602caaa5c784
jumtech <despairing.clock+jumGithub@gmail.com>
2017-03-27 01:42:30 +0900

    ツェルナー錯視を追加

--- data/optical_illusion_set.js
+++ data/optical_illusion_set.js
@@ -3,9 +3,10 @@ import MullerLyer from '../components/optical_illusion/MullerLyer';
 import Ponzo01 from '../components/optical_illusion/Ponzo01';
 import Ponzo02 from '../components/optical_illusion/Ponzo02';
 import Ehrenstein from '../components/optical_illusion/Ehrenstein';
-import Diamond from '../components/optical_illusion/Diamond';
 import Jastrow from '../components/optical_illusion/Jastrow';
+import Diamond from '../components/optical_illusion/Diamond';
 import Poggendorff from '../components/optical_illusion/Poggendorff';
+import Zollner from '../components/optical_illusion/Zollner';
 
 export const optical_illusion_set = [
     { name: 'ミュラリー・リヤー錯視', component: (<MullerLyer />) },
@@ -15,7 +16,7 @@ export const optical_illusion_set = [
     { name: 'ジャストロー錯視', component: (<Jastrow />) },
     { name: '正方形・ダイヤモンド形錯視', component: (<Diamond />) },
     { name: 'ポッケンドルフ錯視', component: (<Poggendorff />) },
-    { name: 'ツェルナー錯視', component: (<MullerLyer />) },
+    { name: 'ツェルナー錯視', component: (<Zollner />) },
     { name: 'ヘリング錯視', component: (<MullerLyer />) },
     { name: 'カフェウォール錯視', component: (<MullerLyer />) },
     { name: 'ずれたグラデーションの錯視', component: (<MullerLyer />) },
